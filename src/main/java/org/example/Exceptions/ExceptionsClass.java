a4f1b22b738cf6843dfee7725778298455819ea8
ArunDrioder <arunprasadh.s@gmail.com>
2024-07-07 20:21:03 +0530

    New Core java Concepts added

---
+++ src/main/java/org/example/Exceptions/ExceptionsClass.java
@@ -0,0 +1,32 @@
+package org.example.Exceptions;
+
+public class ExceptionsClass
+{
+    public static void main(String[] args)
+    {
+        int b=7; int c=0;
+        try {
+            int k=b/c;
+             int arr[]=new int[5];
+            System.out.println(arr[7]);
+             }
+             catch(ArithmeticException et)
+             {
+         System.out.println("I catched the Arthimetic/exception");
+             }
+             catch(IndexOutOfBoundsException ets)
+             {
+             System.out.println("I catched the IndexBound/exception");
+             } catch(Exception e)
+             {
+            System.out.println("I catched the error/exception");
+              }
+              finally
+              {
+            System.out.println("delete cookies");
+            // THis block is executed irrespective of exception thrown or not
+             }
+
+        }
+    }
+
